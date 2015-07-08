1596eaeb341afb343d4f07b3b1baa19ac246ee99
Forty-two <vsprieto@gmail.com>
2015-07-07 21:00:25 -0500

    Can now primitively change the number of subbeats in a beat, togglebuttons instead of regular buttons.

--- polyrhythms.py
+++ polyrhythms.py
@@ -7,7 +7,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton
 from kivy.graphics import Color,Rectangle
 import random
 
@@ -85,9 +85,10 @@ class ChangeableBoxLayout(BoxLayout):
     def __init__(self, no_repeated_classes=None,
                   repeated_class_args = (),repeated_class_kwargs=None,
                   extra_classes_args = None,extra_classes_kwargs=None,
-                 bg_color = None, bg_color_args = {},
+                 bg_color = None, bg_color_args = None,
                 **kwargs):
-
+        if bg_color_args is None:
+            bg_color_args = {}
 
         self.process_inputs_against_class_instances(
                                                no_repeated_classes,
@@ -102,25 +103,45 @@ class ChangeableBoxLayout(BoxLayout):
         # Don't break built-in functionality.
         super(ChangeableBoxLayout,self).__init__(**kwargs)
 
+
+        self.setup_background(bg_color,bg_color_args)
+
         ## possibly implement error checking for arguments, but it's non-trivial.
         ## if you get an error from here,
         ## it's likely because your lists do not match up.
+        self.rpt_widgets = []
         self.add_repeated_class_widgets()
+
+        self.extra_widgets = []
         self.add_extra_classes()
 
         self.ready_for_update = False
 
-        #self.setup_background(bg_color,bg_color_args)
+    def setup_background(self,bg_color,bg_color_args):
+        background = bg_color if bg_color else self.bg_color
+        bg_kwargs = self.bg_color_args.copy()
+        bg_kwargs.update(bg_color_args)
+
+        with self.canvas.before:
+            Color(*background,**bg_kwargs)
+            self.rect = Rectangle(size = self.size,pos=self.pos)
+
+        self.bind(size=self._update_rect,pos = self._update_rect)
 
     def add_repeated_class_widgets(self):
 
         #first add all of the repeated class elements
         for i in range(self.times_rpt_class):
             # noinspection PyArgumentList
-            self.add_widget(self.repeated_class(
-                *self.rpt_args,**self.rpt_kwargs
-                )
-            )
+            class_instance =self.single_rpt_class()
+
+            self.add_widget(class_instance)
+            self.rpt_widgets.append(class_instance)
+
+    def single_rpt_class(self):
+        return self.repeated_class(
+                            *self.rpt_args,**self.rpt_kwargs
+                            )
 
     def add_extra_classes(self):
          #then add all of the extra class elements, with desired arguments
@@ -130,6 +151,8 @@ class ChangeableBoxLayout(BoxLayout):
                 **self.extra_kwargs[extra_class]
                 )
             )
+
+
 
     def process_inputs_against_class_instances(self,
                                                no_repeated_classes,
@@ -189,16 +212,8 @@ class ChangeableBoxLayout(BoxLayout):
         else:
             self.extra_kwargs =  self.extra_classes_kwargs
 
-    def setup_background(self,bg_color,bg_color_args):
-        background = bg_color if bg_color else self.bg_color
-        bg_kwargs = self.bg_color_args.copy()
-        bg_kwargs.update(bg_color_args)
 
-        with self.canvas.before:
-            Color(*background,**bg_kwargs)
-            self.rect = Rectangle(size = self.size,pos=self.pos)
 
-        self.bind(size=self._update_rect,pos = self._update_rect)
 
     def handle_spec_inputs_for_init(self,kwargs):
         '''
@@ -218,15 +233,20 @@ class ChangeableBoxLayout(BoxLayout):
         self.rect.pos = instance.pos
         self.rect.size = instance.size
 
-    def update_grid_size(self,new_no,new_args):
+    def update_grid_size(self,new_no,new_args = None):
         '''
 
         :param new_no: The new number of children it needs
         :param new_args: dict or NoneType
         :return:
         '''
-        for child in self.children[:]:
-            pass
+        #add information to allow for new arguments
+        for repeat_widget in self.rpt_widgets:
+            self.remove_widget(repeat_widget)
+        self.rpt_widgets = []
+
+        self.times_rpt_class = new_no
+        self.add_repeated_class_widgets()
 
 
     def on_touch_up(self, touch):
@@ -265,7 +285,8 @@ class RecursiveBoxLayout(ChangeableBoxLayout):
         return return_dict
 
 
-class SubBeat(Button):
+class SubBeat(ToggleButton):
+    #def on_touch_down(self, touch):
     pass
 
 class Beat(RecursiveBoxLayout):
@@ -273,7 +294,7 @@ class Beat(RecursiveBoxLayout):
     no_repeated_classes = 1
     repeated_class_kwargs = {'orientation':'horizontal',
                              'spacing':10,
-                             #'size_hint':(1,.9)
+                             'pos_hint':{'y':.05},'size_hint':[1.,.9]
                              }
 
     #repeated_class_args = ()
@@ -284,29 +305,46 @@ class Beat(RecursiveBoxLayout):
     desired_kept_vars = ('no_subbeats',)
     map_kept_attrs = {'no_subbeats': 'no_repeated_classes'}
 
-    def add_repeated_class_widgets(self):
-        for i in range(self.times_rpt_class):
-            # noinspection PyArgumentList
-            self.add_widget(self.repeated_class(
-                background_color = (random.random(),.8,.8),
-                *self.rpt_args,**self.rpt_kwargs
+    def setup_background(self,bg_color,bg_color_args):
+        background = bg_color if bg_color else self.bg_color
+        bg_kwargs = self.bg_color_args.copy()
+        bg_kwargs.update(bg_color_args)
 
-                )
-            )
+        with self.canvas.before:
+            Color(*background,**bg_kwargs)
+            self.rect = Rectangle(size = self.size,pos=self.pos)
+
+        self.bind(size=self._update_rect,pos = self._update_rect)
+        self.touch_coords = None
+
 
     def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            if touch.is_double_tap:
+                self.ready_for_update = True
+                self.touch_coords =  touch.x,touch.y
 
-        if touch.is_double_tap:
-            self.ready_for_update = True
-            self.touch_coords =  touch.x,touch.y
-        else:
-            super(ChangeableBoxLayout,self).on_touch_down(touch)
+                for child in self.children[:]:
+                    if child.dispatch('on_touch_down', touch):
+                        break
+                return True
+
+        super(ChangeableBoxLayout,self).on_touch_down(touch)
 
 
     def on_touch_move(self, touch):
-        if self.ready_for_update:
-            prev_x,prev_y = self.touch_coords
-        else:
+        if self.touch_coords and self.collide_point(*self.touch_coords):
+            if self.ready_for_update:
+                prev_x,prev_y = self.touch_coords
+                touch_diff = touch.y-prev_y
+                graininess = self.height/4
+                if abs(touch_diff)>(graininess):
+                    new_grid_size = int(self.times_rpt_class +(touch_diff//graininess))
+                    new_grid_size = max(min(new_grid_size,6),1)
+                    self.update_grid_size(new_grid_size)
+                    return True
+
+
             super(ChangeableBoxLayout,self).on_touch_move(touch)
 
 class Instrument(RecursiveBoxLayout):
@@ -315,6 +353,7 @@ class Instrument(RecursiveBoxLayout):
 
     repeated_class = Beat
     repeated_class_kwargs = {'orientation':'horizontal','spacing':10}
+
     #repeated_class_args = ()
     extra_classes = ()
     extra_classes_kwargs = defaultdict(tuple)
@@ -323,6 +362,13 @@ class Instrument(RecursiveBoxLayout):
     desired_kept_vars = ('no_beats',)#'no_subbeats')
     map_kept_attrs = {'no_beats': 'no_repeated_classes'}
     recursive_kw_args = ('no_subbeats',)
+    def single_rpt_class(self):
+        return self.repeated_class(
+            bg_color = (random.random(),0.4,0.92),
+            *self.rpt_args,**self.rpt_kwargs
+        )
+
+
 
 
 
